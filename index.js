b3ac2a18cfa3e77919d9d5a08af455c90499d553
Rehan Kodekar <mkodekar@zoho.com>
2018-05-05 02:37:02 +0530

    styling elements with array syntax

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/* 
-new Vue({
-  el: "#app2",
-  data: {
-    color: gray,
-    width: 100
-  },
-  computed: {
-    myStyle: function() {
-      return {
-        backgroundColor: this.color,
-        width: this.width + "px"
-      };
-    }
-  }
-}); */
-
 new Vue({
   el: "#app",
   data: {
